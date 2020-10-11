0e90a08fc0d98982044f04330801b824b8d053b4
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-11 07:59:32 -0700

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const { PORT } = require('./config')
+
+// const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
