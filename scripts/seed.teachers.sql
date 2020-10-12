89d0dfcbb784cc3a0ab33a0339cfd1541153d549
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-12 08:46:05 -0700

    Added data to the tables

---
+++ scripts/seed.teachers.sql
@@ -0,0 +1,7 @@
+INSERT INTO teachers (id, first_name, last_name)
+VALUES (0, 'admin', 'admin'),
+  (1, 'Alex', 'Jackson'),
+  (2, 'Beverly', 'Dickson'),
+  (3, 'Chris', 'Wickson'),
+  (4, 'Dennis', 'Figure'),
+  (5, 'Elizabeth', 'Emblem');
