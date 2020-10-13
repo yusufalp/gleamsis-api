fe026951635e42a0f4ce01473af82e990bf17aed
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-13 13:34:48 -0700

    Updated the seed document

--- scripts/seed.courses.sql
+++ scripts/seed.courses.sql
@@ -1,11 +1,11 @@
-INSERT INTO courses (id, name, category, teacher_id)
-VALUES (1, 'English 1', 'ELA', 1),
-  (2, 'English 2', 'ELA', 1),
-  (3, 'Algebra 1', 'Math', 2),
-  (4, 'Algebra 2', 'Math', 2),
-  (5, 'Geometry', 'Math', 2),
-  (6, 'Biology', 'Science', 3),
-  (7, 'Chemistry', 'Science', 4),
-  (8, 'Physics', 'Science', 4),
-  (9, 'US History', 'History', 5),
-  (10, 'World History', 'History', 5);
+INSERT INTO courses (name, category, teacher_id)
+VALUES ('English 1', 'ELA', 1),
+  ('English 2', 'ELA', 1),
+  ('Algebra 1', 'Math', 2),
+  ('Algebra 2', 'Math', 2),
+  ('Geometry', 'Math', 2),
+  ('Biology', 'Science', 3),
+  ('Chemistry', 'Science', 4),
+  ('Physics', 'Science', 4),
+  ('US History', 'History', 5),
+  ('World History', 'History', 5);
--- scripts/seed.teachers.sql
+++ scripts/seed.teachers.sql
@@ -1,7 +1,6 @@
-INSERT INTO teachers (id, first_name, last_name)
-VALUES (0, 'admin', 'admin'),
-  (1, 'Alex', 'Jackson'),
-  (2, 'Beverly', 'Dickson'),
-  (3, 'Chris', 'Wickson'),
-  (4, 'Dennis', 'Figure'),
-  (5, 'Elizabeth', 'Emblem');
+INSERT INTO teachers (first_name, last_name)
+VALUES ('Alex', 'Jackson'),
+  ('Beverly', 'Dickson'),
+  ('Chris', 'Wickson'),
+  ('Dennis', 'Figure'),
+  ('Elizabeth', 'Emblem');
