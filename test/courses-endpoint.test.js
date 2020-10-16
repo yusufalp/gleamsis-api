bbccf8afaea8456a4ed30d8fb6621741c37ee913
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-16 12:28:23 -0700

    Added test cases

---
+++ test/courses-endpoint.test.js
@@ -0,0 +1,159 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const app = require('../src/app');
+
+describe('Courses endpoint', () => {
+  db = knex({
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL
+  })
+  app.set('db', db)
+
+  const testTeachers = [
+    {
+      id: 1,
+      first_name: 'admin',
+      last_name: 'admin'
+    },
+    {
+      id: 2,
+      first_name: 'Alex',
+      last_name: 'Jackson'
+    },
+    {
+      id: 3,
+      first_name: 'Chris',
+      last_name: 'Wickson'
+    },
+  ]
+
+  after('disconnect from db', () => {
+    return db.destroy()
+  })
+
+  before('clean the table', () => db.raw('TRUNCATE courses,teachers RESTART IDENTITY CASCADE'))
+
+  afterEach('cleanup', () => db.raw('TRUNCATE courses RESTART IDENTITY CASCADE'))
+
+  before('insert teachers', () => {
+    return db
+      .into('teachers')
+      .insert(testTeachers)
+  })
+
+  describe('GET /api/courses', () => {
+    context('Given there are no data', () => {
+      it('responds with 200', () => {
+        return supertest(app)
+          .get('/api/courses')
+          .expect(200, [])
+      })
+    })
+
+    context('Given there are data in the courses table', () => {
+      const testCourses = [
+        {
+          id: 1,
+          name: 'English 1',
+          category: 'ELA',
+          teacher_id: 1
+        },
+        {
+          id: 2,
+          name: 'English 2',
+          category: 'ELA',
+          teacher_id: 1
+        },
+        {
+          id: 3,
+          name: 'Algebra 1',
+          category: 'Math',
+          teacher_id: 2
+        },
+      ]
+
+      beforeEach('insert courses', () => {
+        return db
+          .into('courses')
+          .insert(testCourses)
+      })
+
+      it('responds with 200 and with all courses', () => {
+        return supertest(app)
+          .get('/api/courses')
+          .expect(200, testCourses)
+      })
+    })
+  })
+
+  describe('POST /api/courses', () => {
+    it('creates a course responding with 201 and the new course', function () {
+      const newCourse = {
+        name: 'Test name',
+        category: 'Test category',
+        teacher_id: 2
+      }
+      return supertest(app)
+        .post('/api/courses')
+        .send(newCourse)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.name).to.eql(newCourse.name)
+          expect(res.body.category).to.eql(newCourse.category)
+          expect(res.body.teacher_id).to.eql(newCourse.teacher_id)
+          expect(res.body).to.have.property('id')
+        })
+    })
+  })
+  describe('DELETE /api/courses/:id', () => {
+    context('Given there are no courses', () => {
+      it('responds with 404', () => {
+        const courseId = 123456
+        return supertest(app)
+          .delete(`/api/courses/${courseId}`)
+          .expect(404, { error: { message: 'Course does not exist' } })
+      })
+    })
+    context('Given there are courses in the database', () => {
+      const testCourses = [
+        {
+          id: 1,
+          name: 'English 1',
+          category: 'ELA',
+          teacher_id: 1
+        },
+        {
+          id: 2,
+          name: 'English 2',
+          category: 'ELA',
+          teacher_id: 1
+        },
+        {
+          id: 3,
+          name: 'Algebra 1',
+          category: 'Math',
+          teacher_id: 2
+        },
+      ]
+
+      beforeEach('insert courses', () => {
+        return db
+          .into('courses')
+          .insert(testCourses)
+      })
+
+      it('responds with 204 and removes the course', () => {
+        const idToRemove = 2
+        const expectedCourse = testCourses.filter(course => course.id !== idToRemove)
+        return supertest(app)
+          .delete(`/api/courses/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get('/api/courses')
+              .expect(expectedCourse)
+          )
+      })
+    })
+  })
+})
---
+++ test/students-endpoint.test.js
@@ -0,0 +1,196 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const app = require('../src/app');
+
+describe('Students endpoint', () => {
+  before('connection to database', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL
+    })
+    app.set('db', db)
+  })
+
+
+  after('disconnect from db', () => {
+    return db.destroy()
+  })
+
+  before('clean the table', () => db.raw('TRUNCATE students, teachers RESTART IDENTITY CASCADE'))
+
+  const testCourses = [
+    {
+      id: 1,
+      name: 'English 1',
+      category: 'ELA',
+      teacher_id: 1
+    },
+    {
+      id: 2,
+      name: 'English 2',
+      category: 'ELA',
+      teacher_id: 1
+    },
+    {
+      id: 3,
+      name: 'Algebra 1',
+      category: 'Math',
+      teacher_id: 2
+    },
+  ]
+  const testTeachers = [
+    {
+      id: 1,
+      first_name: 'admin',
+      last_name: 'admin'
+    },
+    {
+      id: 2,
+      first_name: 'Alex',
+      last_name: 'Jackson'
+    },
+    {
+      id: 3,
+      first_name: 'Chris',
+      last_name: 'Wickson'
+    },
+  ]
+
+  afterEach('cleanup', () => db.raw('TRUNCATE students RESTART IDENTITY CASCADE'))
+
+  before('insert teachers', () => {
+    return db
+      .into('teachers')
+      .insert(testTeachers)
+  })
+
+  before('insert courses', () => {
+    return db
+      .into('courses')
+      .insert(testCourses)
+  })
+
+  describe('GET /api/students', () => {
+    context('Given there are no data', () => {
+      it('responds with 200', () => {
+        return supertest(app)
+          .get('/api/students')
+          .expect(200, [])
+      })
+    })
+
+    context('Given there are data in the students table', () => {
+      const testStudents = [
+        {
+          id: 1,
+          first_name: 'Gabriel',
+          last_name: 'Aqua',
+          course_id: 1,
+          grade: 'A'
+        },
+        {
+          id: 2,
+          first_name: 'Jose',
+          last_name: 'Dick',
+          course_id: 1,
+          grade: 'B'
+        },
+        {
+          id: 3,
+          first_name: 'Stephanie',
+          last_name: 'Vicki',
+          course_id: 1,
+          grade: 'A'
+        },
+      ]
+
+      beforeEach('insert students', () => {
+        return db
+          .into('students')
+          .insert(testStudents)
+      })
+
+      it('responds with 200 and with all students', () => {
+        return supertest(app)
+          .get('/api/students')
+          .expect(200, testStudents)
+      })
+    })
+  })
+
+  describe('POST /api/students', () => {
+    it('creates a student responding with 201 and the new student', function () {
+      const newStudent = {
+        first_name: 'Test firstName',
+        last_name: 'Test lastName',
+        course_id: 1,
+        grade: 'A'
+      }
+      return supertest(app)
+        .post('/api/students')
+        .send(newStudent)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.first_name).to.eql(newStudent.first_name)
+          expect(res.body.last_name).to.eql(newStudent.last_name)
+          expect(res.body.course_id).to.eql(newStudent.course_id)
+          expect(res.body.grade).to.eql(newStudent.grade)
+          expect(res.body).to.have.property('id')
+        })
+    })
+  })
+  describe('DELETE /api/students/:id', () => {
+    context('Given there are no students', () => {
+      it('responds with 404', () => {
+        const studentId = 123456
+        return supertest(app)
+          .delete(`/api/students/${studentId}`)
+          .expect(404, { error: { message: 'Student does not exist' } })
+      })
+    })
+    context('Given there are students in the database', () => {
+      const testStudents = [
+        {
+          id: 1,
+          first_name: 'Gabriel',
+          last_name: 'Aqua',
+          course_id: 1,
+          grade: 'A'
+        },
+        {
+          id: 2,
+          first_name: 'Jose',
+          last_name: 'Dick',
+          course_id: 1,
+          grade: 'B'
+        },
+        {
+          id: 3,
+          first_name: 'Stephanie',
+          last_name: 'Vicki',
+          course_id: 1,
+          grade: 'A'
+        },
+      ]
+
+      beforeEach('insert students', () => {
+        return db
+          .into('students')
+          .insert(testStudents)
+      })
+
+      it('responds with 204 and removes the student', () => {
+        const idToRemove = 2
+        const expectedStudent = testStudents.filter(student => student.id !== idToRemove)
+        return supertest(app)
+          .delete(`/api/students/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get('/api/students')
+              .expect(expectedStudent)
+          )
+      })
+    })
+  })
+})
