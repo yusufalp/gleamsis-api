a287a8f7aa5b5aaf7a8b696871c72c0f89c18f27
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-13 21:59:19 -0800

    Add basic authorization

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,6 +1,22 @@
 function requireAuth(req, res, next) {
-  console.log('requireAuth')
-  console.log(req.get('Authorization'))
+  const authToken = req.get('Authorization') || ''
+
+  let basicToken;
+  if (authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' })
+  } else {
+    basicToken = authToken.slice('basic '.length, authToken.length);
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer
+    .from(basicToken, 'base64')
+    .toString()
+    .split(':')
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
   next()
 }
 
